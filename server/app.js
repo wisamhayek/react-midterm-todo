8695a107c43ed6a45f5d08c2ede501e75f99303e
Wisam Hayek <hayekwisam@gmail.com>
2022-05-30 17:28:38 -0700

    updated PORT

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose  = require('mongoose');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const mongoURl = process.env.MONGODB_URL;
 const cors = require('cors')
 const morgan = require('morgan');
